cf94d4979b972527a080cc0cc1e3e66b709cf869
iqbal.muhammad <Tapiiqbal93>
2019-10-19 18:52:17 +0700

    change http to https

--- dev-quasar-vue/src/layouts/Beranda.vue
+++ dev-quasar-vue/src/layouts/Beranda.vue
@@ -55,6 +55,16 @@
                 Drafts
               </q-item-section>
             </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="drafts" />
+              </q-item-section>
+
+              <q-item-section>
+                Drafts
+              </q-item-section>
+            </q-item>
           </q-list>
         </q-scroll-area>
 
